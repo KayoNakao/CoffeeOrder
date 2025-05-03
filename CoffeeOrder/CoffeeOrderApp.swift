6966d1c791feea96b897bcde6078574a0b8cec6d
Kayo Nakao <kayo.nakao87@gmail.com>
2025-05-03 12:21:56 -0400

    Initial commit

---
+++ CoffeeOrder/CoffeeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderApp.swift
+//  CoffeeOrder
+//
+//  Created by Kayo on 2025-05-03.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
